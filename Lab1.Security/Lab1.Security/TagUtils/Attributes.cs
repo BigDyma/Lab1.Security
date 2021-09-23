2aef769399c6e9884c9eb3bd7b0a42ca4673e348
Dumitru Strelet <dumitru.strelet@amdaris.com>
2021-09-23 19:18:17 +0300

    Need to serialize json@

--- Lab1.Security/Lab1.Security/TagUtils/Attributes.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lab1.Security
-{
-    public  class Attributes
-    {
-        public string Name { get; set; }
-
-        public string Value { get; set; }
-    }
-}
--- Lab1.Security/Program.cs
+++ Lab1.Security/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Text.Json;
 
 namespace Lab1.Security
 {
@@ -10,6 +10,9 @@ namespace Lab1.Security
             AuditFile audit = new AuditFile(@"C:\Users\dumitru.strelet\source\repos\Lab1.Security\Lab1.Security\Lib\CIS_Cisco_IOS_15_v4.1.0_Level_1.audit");
 
             var result = audit.ResultJSON;
+
+            var finalresult = JsonSerializer.Serialize(result);
+
         }
     }
 }
